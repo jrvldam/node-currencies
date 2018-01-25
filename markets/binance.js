c32ae22f223d5c7fc9c3df4c4cd04f7bc59f9de9
rodrigo.vargas <jrvargas@atsistemas.com>
2018-01-25 18:02:55 +0100

    Send binance currencies price

--- markets/binance.js
+++ markets/binance.js
@@ -20,22 +20,34 @@ module.exports = {
     return this.cache;
   },
   updateCache() {
-    fetch(`${URL_DOMAIN}/api/v1/exchangeInfo`)
-      .then(response => {
+    const priceTicker = fetch(`${URL_DOMAIN}/api/v3/ticker/price`);
+    const exchangeInfo = fetch(`${URL_DOMAIN}/api/v1/exchangeInfo`);
+    Promise.all([exchangeInfo, priceTicker])
+      .then(values => {
+        const data = values[0].symbols;
+        const prices = getPriceDictionary(values[1]);
         this.cache = {
           name: 'Binance',
-          data: mapToCustomArray(response.symbols),
+          data: mapToCustomArray(data, prices),
           ts: Date.now(),
         };
       });
   }
 };
 
-function mapToCustomArray(data = []) {
+function getPriceDictionary(priceTicker) {
+  return priceTicker.reduce((acc, coin) => {
+    acc[coin.symbol] = coin.price;
+    return acc;
+  }, {});
+}
+
+function mapToCustomArray(data = [], prices) {
   return data.map((coin, index) => ({
     id: coin.symbol,
     symbol: coin.symbol,
     name: coin.baseAsset,
+    price: prices[coin.symbol],
     order: index,
   }));
 }
